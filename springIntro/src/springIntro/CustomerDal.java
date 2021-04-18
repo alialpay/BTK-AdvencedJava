6472a5ed19ef82083e5d0873a290953333f435ea
Ali Alpay <alialpay96@hotmail.com>
2021-04-19 01:22:51 +0300

    First Commit

---
+++ springIntro/src/springIntro/CustomerDal.java
@@ -0,0 +1,8 @@
+package springIntro;
+
+public class CustomerDal implements ICustomerDal {
+
+	public void add() {
+		System.out.println("Oracle veritabanýna eklendi");
+	}
+}
---
+++ springIntro/src/springIntro/MySqlCustomerDal.java
@@ -0,0 +1,8 @@
+package springIntro;
+
+public class MySqlCustomerDal implements ICustomerDal {
+
+	public void add() {
+		System.out.println("MysSql veritabanýna eklendi");
+	}
+}
