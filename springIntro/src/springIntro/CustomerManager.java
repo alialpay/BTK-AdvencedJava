4665325d290077ab7ddb88b71fce20d6db6522a5
Ali Alpay <alialpay96@hotmail.com>
2021-04-21 01:20:13 +0300

    setterInjection

--- springIntro/src/springIntro/CustomerManager.java
+++ springIntro/src/springIntro/CustomerManager.java
@@ -4,9 +4,16 @@ public class CustomerManager implements ICustomerService {
 
 	private ICustomerDal customerDal;
 	
-	public CustomerManager(ICustomerDal customerDal) {
+	//constructor injection
+//	public CustomerManager(ICustomerDal customerDal) {
+//		this.customerDal = customerDal;
+//	}
+	
+	//setter injection (tercih edilen yöntem constructor injection)
+	public void setCustomerDal(ICustomerDal customerDal) {
 		this.customerDal = customerDal;
 	}
+
 	public void add() {
 		
 		customerDal.add();
