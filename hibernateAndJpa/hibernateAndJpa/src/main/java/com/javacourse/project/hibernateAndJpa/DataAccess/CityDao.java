de401e3deab94bfcc06934fa05f5907e276d1e6c
Ali Alpay <alialpay96@hotmail.com>
2022-02-05 06:02:00 +0300

    dao

---
+++ hibernateAndJpa/hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/DataAccess/CityDao.java
@@ -0,0 +1,12 @@
+package com.javacourse.project.hibernateAndJpa.DataAccess;
+
+import java.util.List;
+
+import com.javacourse.project.hibernateAndJpa.Entities.City;
+
+public interface CityDao {
+	List<City> getAll();
+	void add(City city);
+	void update(City city);
+	void delete(City city);
+}
