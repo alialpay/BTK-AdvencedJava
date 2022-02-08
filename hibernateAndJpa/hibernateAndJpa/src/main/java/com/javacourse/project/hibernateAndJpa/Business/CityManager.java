a4eadbdf0fec5ac34d660e345cbc62e35e799177
Ali Alpay <alialpay96@hotmail.com>
2022-02-08 21:56:22 +0300

    business

---
+++ hibernateAndJpa/hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/Business/CityManager.java
@@ -0,0 +1,49 @@
+package com.javacourse.project.hibernateAndJpa.Business;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javacourse.project.hibernateAndJpa.DataAccess.CityDao;
+import com.javacourse.project.hibernateAndJpa.Entities.City;
+
+@Service
+public class CityManager implements CityService {
+
+	private CityDao cityDao;
+	
+	@Autowired
+	public CityManager(CityDao cityDao) {
+		this.cityDao = cityDao;
+	}
+
+	@Override
+	@Transactional
+	public List<City> getAll() {
+		return this.cityDao.getAll();
+	}
+
+	@Override
+	@Transactional
+	public void add(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@Transactional
+	public void update(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@Transactional
+	public void delete(City city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
